3a5f391b7108ed6c9ab0cf8ad3822b7bb5961a10
techpassel <techpasselorg@gmail.com>
2022-12-12 13:56:05 +0530

    Completed till Hero section

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import { styles } from '../constants/style';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
--- frontend/components/GetStarted.tsx
+++ frontend/components/GetStarted.tsx
@@ -1,8 +1,23 @@
+import Image from 'next/image'
 import React from 'react'
+import { styles } from '../constants/style'
+import { arrowUp } from '../public'
 
 const GetStarted = () => {
   return (
-    <div>GetStarted</div>
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+      <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+        <div className={`${styles.flexStart} flex-row`}>
+          <p className='font-poppins font-medium text-[18px] leading-[23px] text-gradient mr-2'>
+            Get
+          </p>
+          <Image src={arrowUp} alt="arrow" width={23} height={23} className="object-contain" />
+        </div>
+        <p className='font-poppins font-medium text-[18px] leading-[23px] text-gradient'>
+          Started
+        </p>
+      </div>
+    </div>
   )
 }
 
