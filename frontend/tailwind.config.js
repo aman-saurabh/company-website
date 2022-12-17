c85379a6ae2a97db05017f1b508def296b35badd
techpassel <techpasselorg@gmail.com>
2022-12-17 12:51:23 +0530

    added style.js in tailwind config file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,8 +2,8 @@ module.exports = {
     content: [
         './app/*.{ts,tsx}',
         './app/**/*.{ts,tsx}',
-        './pages/**/*.{ts,tsx}',
         './components/*.{ts,tsx}',
+        './constants/*.{js}'
         // For the best performance and to avoid false positives,
         // be as specific as possible with your content configuration.
     ],
